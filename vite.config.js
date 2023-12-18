38415d6620465a0f59a5b0631f790b09de383abb
Precious Betine <preciousbetine@gmail.com>
2023-12-18 17:14:50 +0100

    Update site title and favicon

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/categories': 'http://localhost:3000',
-      '/admin': 'http://localhost:3000',
-      '/minister': 'http://localhost:3000',
-      '/ministers': 'http://localhost:3000',
-      '/articles': 'http://localhost:3000',
-      '/draft': 'http://localhost:3000',
-      '/search': 'http://localhost:3000',
-      '/confirm': 'http://localhost:3000',
-      '/ask': 'http://localhost:3000',
-      '/uploads': 'http://localhost:3000',
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/categories': 'http://localhost:3000',
+  //     '/admin': 'http://localhost:3000',
+  //     '/minister': 'http://localhost:3000',
+  //     '/ministers': 'http://localhost:3000',
+  //     '/articles': 'http://localhost:3000',
+  //     '/draft': 'http://localhost:3000',
+  //     '/search': 'http://localhost:3000',
+  //     '/confirm': 'http://localhost:3000',
+  //     '/ask': 'http://localhost:3000',
+  //     '/uploads': 'http://localhost:3000',
+  //   }
+  // }
 })

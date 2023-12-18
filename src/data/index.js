6aa1f1fd71362967bd1d68a67de406b92de4eb13
Precious Betine <preciousbetine@gmail.com>
2023-12-18 15:08:01 +0100

    Update base url

--- src/data/index.js
+++ src/data/index.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/',
+    // baseUrl: '/',
+    baseUrl: 'https://blog.api.thekingdomtrybe.com',
     credentials: 'include',
   }),
   endpoints: () => ({

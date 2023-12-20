005c1bf5e21050da7a8d762b473f609556150f08
Precious Betine <preciousbetine@gmail.com>
2023-12-20 08:19:35 +0100

    Update RTK base query URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  // server: {
     // proxy: {
     //   '/categories': 'http://localhost:3000',
     //   '/admin': 'http://localhost:3000',
@@ -17,17 +17,17 @@ export default defineConfig({
     //   '/ask': 'http://localhost:3000',
     //   '/uploads': 'http://localhost:3000',
     // }
-    proxy: {
-      '/categories': 'https://blog.api.thekingdomtrybe.com',
-      '/admin': 'https://blog.api.thekingdomtrybe.com',
-      '/minister': 'https://blog.api.thekingdomtrybe.com',
-      '/ministers': 'https://blog.api.thekingdomtrybe.com',
-      '/articles': 'https://blog.api.thekingdomtrybe.com',
-      '/draft': 'https://blog.api.thekingdomtrybe.com',
-      '/search': 'https://blog.api.thekingdomtrybe.com',
-      '/confirm': 'https://blog.api.thekingdomtrybe.com',
-      '/ask': 'https://blog.api.thekingdomtrybe.com',
-      '/uploads': 'https://blog.api.thekingdomtrybe.com',
-    }
-  }
+    // proxy: {
+    //   '/categories': 'https://blog.api.thekingdomtrybe.com',
+    //   '/admin': 'https://blog.api.thekingdomtrybe.com',
+    //   '/minister': 'https://blog.api.thekingdomtrybe.com',
+    //   '/ministers': 'https://blog.api.thekingdomtrybe.com',
+    //   '/articles': 'https://blog.api.thekingdomtrybe.com',
+    //   '/draft': 'https://blog.api.thekingdomtrybe.com',
+    //   '/search': 'https://blog.api.thekingdomtrybe.com',
+    //   '/confirm': 'https://blog.api.thekingdomtrybe.com',
+    //   '/ask': 'https://blog.api.thekingdomtrybe.com',
+    //   '/uploads': 'https://blog.api.thekingdomtrybe.com',
+    // }
+  // }
 })
